ae08d9aff56010da7f39cd7ac2f03c501ae1e42e
Gabe Alzate <ggalzate98@gmail.com>
2019-02-09 14:59:25 -0800

    First commit

---
+++ signIO/main.js
@@ -0,0 +1,9 @@
+function goToPage(){
+
+    if(document.getElementById('signOut').checked){
+        window.location.href = 'https://creator.zohopublic.com/patriciadeehring/middle-tree-sign-in-out/form-embed/Middle_Tree_Sign_Out/UugB0fnTu5xdXkPWNRR4YZD9kPNpC7OEAzz4vDDW13RJW929UYuDaQ4ksVNS90P1n7PANFjrOzEqzbmxuUYV3F15EUSmMwkrh1sy';
+    }
+    else
+        window.location.href = 'https://creator.zohopublic.com/patriciadeehring/middle-tree-sign-in-out/form-embed/Sign_In1/jsnkCFSFeevsQr9HKZt87sDKVQJRjh0hfjCKqxxbugB2NtU77vaPzpCAMEKxzjS7evCvbP2CP1xADjSHS3jBC9B1NTgERz37mx5G';
+
+}
